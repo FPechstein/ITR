c973f8fb1c11ac45cbc1884f628d318efaca11b1
drivingswarm <fabian.pechstein@st.ovgu.de>
2022-04-30 23:18:05 +0200

    hallo

--- src/reactive_behaviour/reactive_behaviour/controller.py
+++ src/reactive_behaviour/reactive_behaviour/controller.py
@@ -1,4 +1,5 @@
 import rclpy
+import time
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
@@ -10,21 +11,58 @@ class VelocityController(Node):
         super().__init__('velocity_controller')
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.forward_distance = 0
+        self.right_distance = 0
         self.create_subscription(LaserScan, 'scan', self.laser_cb, rclpy.qos.qos_profile_sensor_data)
         self.create_timer(0.1, self.timer_cb)
         self.get_logger().info('controller node started')
+        self.t_time=time.time()
+        self.t_time2=0
+        self.end_time=0
+        self.flag=0
+        self.z=0.3
+        self.timer=180
         
     def timer_cb(self):
+    	
         msg = Twist()
-        x = self.forward_distance - 0.3
-        x = x if x < 0.1 else 0.1
-        x = x if x >= 0 else 0.0
-        msg.linear.x = x
-        self.publisher.publish(msg)
-    
-    def laser_cb(self, msg):
-        self.forward_distance = msg.ranges[0]
+        self.flag=0
+        #self.get_logger().info({x})
+        self.t_time2=time.time()
+        self.end_time=self.t_time2-self.t_time
+        self.get_logger().info(f'self.timer: {self.timer}')
+        
+        if self.end_time >= self.timer:
+        	self.z=self.z+0.25
+        	self.t_time=time.time()
+        	self.timer=self.timer*0.85
+        	
+        y = self.right_distance -self.z + 0.15
+        x = self.forward_distance - self.z
+        
+        if x > 0.12:
+        	msg.linear.x = 0.12
+        	self.publisher.publish(msg)
+        	self.get_logger().info('move no obj')
+        if x <= 0.12:
+        	self.flag=1
+        	msg.angular.z= 0.25
+        	self.publisher.publish(msg)
+        	self.get_logger().info('move cause objective infront')
+        if y <= 0.1 and self.flag != 1:
+        	self.get_logger().info('right side too close')
+        	msg.angular.z=0.04
+        	self.publisher.publish(msg)
+        	
+        if y >0.12 and y <0.3 and self.flag != 1:
+        	msg.angular.z= -0.2
+        	self.get_logger().info('right side far but not too far away')
+        	self.publisher.publish(msg)
 
+
+    def laser_cb(self, msg):
+        self.forward_distance =min(msg.ranges[:20]+msg.ranges[-20:])
+        self.right_distance= min(msg.ranges[:85]+msg.ranges[95:])
+	 
 
 
 def main(args=None):
