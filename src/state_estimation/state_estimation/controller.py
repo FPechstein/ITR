ae6ce027e8eade9b2f661cf887d93a536a36fd73
drivingswarm <fabian.pechstein@st.ovgu.de>
2022-06-01 14:10:13 +0200

    commit

--- src/state_estimation/state_estimation/controller.py
+++ src/state_estimation/state_estimation/controller.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+import time
 
 from geometry_msgs.msg import Twist, PoseStamped, PointStamped
 from sensor_msgs.msg import LaserScan
@@ -17,26 +18,86 @@ class VelocityController(Node):
         self.create_subscription(PointStamped, 'position', self.position_cb, 10)
         self.create_timer(0.1, self.timer_cb)
         self.get_logger().info('controller node started')
+        self.t_time=time.time()
+        self.t_time2=0
+        self.end_time=0
+        self.flag=0
+        self.z=0.35
+        self.timer=140
+        self.right_distance = 0
+        self.counter=0
+        self.positions=[]
+        self.realdirection
         
     def timer_cb(self):
         msg = Twist()
-        x = self.forward_distance - 0.3
-        x = x if x < 0.1 else 0.1
-        x = x if x >= 0 else 0.0
-        msg.linear.x = x
-        self.publisher.publish(msg)
+        self.flag=0
+        #self.get_logger().info({x})
+        self.t_time2=time.time()
+        self.end_time=self.t_time2-self.t_time
+        self.get_logger().info(f'self.timer: {self.timer}')
+        
+        if self.end_time >= self.timer:
+        	self.z=self.z+0.3
+        	self.t_time=time.time()
+        	self.timer=self.timer*0.75
+        	
+        y = self.right_distance -self.z + 0.05
+        x = self.forward_distance - self.z
+        
+        if x > 0.14:
+        	self.counter=self.counter+1
+        	msg.linear.x = 0.12
+        	self.publisher.publish(msg)
+        	self.get_logger().info('move no obj')
+        if x <= 0.14 and y>0.12:
+        	self.counter=0
+        	self.flag=1
+        	msg.angular.z= -0.1
+        	self.publisher.publish(msg)
+        	self.get_logger().info('move cause objective infront')
+        if x <= 0.14 and y<0.12:
+        	self.counter=0
+        	self.flag=1
+        	msg.angular.z=0.1
+        	self.publisher.publish
+        if y <= 0.1 and self.flag != 1:
+        	self.counter=0
+        	self.get_logger().info('right side too close')
+        	msg.angular.z=0.06
+        	self.publisher.publish(msg)
+        if y >0.15 and self.flag != 1:
+        	self.counter=0
+        	msg.angular.z= -0.2
+        	self.get_logger().info('right side far but not too far away')
+        	self.publisher.publish(msg)
     
     def goal_cb(self, msg):
         goal = msg.pose.position.x, msg.pose.position.y
+        
         if self.goal != goal:
             self.get_logger().info(f'received a new goal: (x={goal[0]}, y={goal[1]})')
             self.goal = goal
     
     def laser_cb(self, msg):
-        self.forward_distance = msg.ranges[0]
+        self.forward_distance =min(msg.ranges[:30]+msg.ranges[-30:])
+        self.right_distance= min(msg.ranges[:80]+msg.ranges[110:])
         
     def position_cb(self, msg):
         self.position = msg.point.x, msg.point.y
+        self.positions.append(msg.point.x,msg.point.y)
+        if len(self.positions)>=3:
+        	direction1=self.positions[len(self.positions-1)]-self.positions[len(self.positions-2)]
+        	direction2=self.positions[len(self.positions-2)]-self.positions[len(self.positions-3)]
+        	self.realdirection=np.mean(direction1,direction2)
+        	
+        
+        
+        if counter <=0:
+        	self.positions.clear()
+        
+        
+        
 
 
 def main(args=None):
--- src/state_estimation/state_estimation/locator.py
+++ src/state_estimation/state_estimation/locator.py
@@ -12,17 +12,36 @@ class LocatorNode(Node):
         super().__init__('locator_node')
         self.anchor_ranges = []
         self.create_subscription(Range, 'range', self.range_cb, 10)
+        #self.create_subscription(Range, 'anchor', self.range_cb, 10)
         self.position_pub = self.create_publisher(PointStamped, 'position', 10)
         self.initialized = False
         self.create_timer(1.0, self.timer_cb)
         self.get_logger().info('locator node started')
-        
+        self.d=0
+        self.a=0
+        self.x2=0
+        self.x3=0
+        self.y2=0
+        self.y3=0
+        self.x4=0
+        self.y4=0
+        self.schnittpunktey=[]
+        self.schnittpunktex=[]
+        self.x=0.0
+        self.y=0.0
+        self.x1=0.0
+        self.y1=0.0
+	
+	
     def range_cb(self, msg):
         self.anchor_ranges.append(msg)
         self.anchor_ranges = self.anchor_ranges[-10:]
+        #for r in self.anchor_ranges:
+        #	self.get_logger().info(f'{r.anchor.x}')
         if not self.initialized:
             self.initialized = True
             self.get_logger().info('first range received')
+            
 
     def timer_cb(self):
         if not self.initialized:
@@ -33,12 +52,77 @@ class LocatorNode(Node):
         self.position_pub.publish(msg)
     
     def calculate_position(self):
-        if not len(self.anchor_ranges):
+        if len(self.anchor_ranges)<5:
             return 0.0, 0.0, 0.0
         
         # YOUR CODE GOES HERE:
-        x = np.mean([r.range for r in self.anchor_ranges]) - 0.5
-        return x, 0.0, 0.0
+         
+        self.d=np.sqrt((self.anchor_ranges[2].anchor.x-self.anchor_ranges[1].anchor.x)**2+(self.anchor_ranges[2].anchor.y-self.anchor_ranges[1].anchor.y)**2)
+        self.a=(self.anchor_ranges[1].range**2-self.anchor_ranges[2].range**2+self.d**2)/(2*self.d)
+        self.h=np.sqrt(self.anchor_ranges[1].range**2-self.a**2)
+        self.x2=self.anchor_ranges[1].anchor.x+self.a*(self.anchor_ranges[2].anchor.x-self.anchor_ranges[1].anchor.x)/self.d
+        self.y2=self.anchor_ranges[1].anchor.y+self.a*(self.anchor_ranges[2].anchor.y-self.anchor_ranges[1].anchor.y)/self.d
+        self.x3=self.x2+self.h*(self.anchor_ranges[2].anchor.y-self.anchor_ranges[1].anchor.y)/self.d
+        self.y3=self.y2-self.h*(self.anchor_ranges[2].anchor.x-self.anchor_ranges[1].anchor.x)/self.d
+        self.x4=self.x2-self.h*(self.anchor_ranges[2].anchor.y-self.anchor_ranges[1].anchor.y)/self.d
+        self.y4=self.y2+self.h*(self.anchor_ranges[2].anchor.x-self.anchor_ranges[1].anchor.x)/self.d
+        
+        self.schnittpunktex.append(self.x3)
+        self.schnittpunktex.append(self.x4)
+        self.schnittpunktey.append(self.y3)
+        self.schnittpunktey.append(self.y4)
+        
+        self.d=np.sqrt((self.anchor_ranges[4].anchor.x-self.anchor_ranges[3].anchor.x)**2+(self.anchor_ranges[4].anchor.y-self.anchor_ranges[3].anchor.y)**2)
+        self.a=(self.anchor_ranges[4].range**2-self.anchor_ranges[3].range**2+self.d**2)/(2*self.d)
+        self.h=np.sqrt(self.anchor_ranges[3].range**2-self.a**2)
+        self.x2=self.anchor_ranges[3].anchor.x+self.a*(self.anchor_ranges[4].anchor.x-self.anchor_ranges[3].anchor.x)/self.d
+        self.y2=self.anchor_ranges[3].anchor.y+self.a*(self.anchor_ranges[4].anchor.y-self.anchor_ranges[3].anchor.y)/self.d
+        self.x3=self.x2+self.h*(self.anchor_ranges[4].anchor.y-self.anchor_ranges[3].anchor.y)/self.d
+        self.y3=self.y2-self.h*(self.anchor_ranges[4].anchor.x-self.anchor_ranges[3].anchor.x)/self.d
+        self.x4=self.x2-self.h*(self.anchor_ranges[4].anchor.y-self.anchor_ranges[3].anchor.y)/self.d
+        self.y4=self.y2+self.h*(self.anchor_ranges[4].anchor.x-self.anchor_ranges[3].anchor.x)/self.d
+        
+        self.schnittpunktex.append(self.x3)
+        self.schnittpunktex.append(self.x4)
+        self.schnittpunktey.append(self.y3)
+        self.schnittpunktey.append(self.y4)
+        
+        
+        if self.schnittpunktex[0]==self.schnittpunktex[1] or self.schnittpunktex[0]==self.schnittpunktex[2] or self.schnittpunktex[0]==self.schnittpunktex[3]:
+        	self.x=self.schnittpunktex[0]
+        elif self.schnittpunktex[2]==self.schnittpunktex[1] or self.schnittpunktex[1]==self.schnittpunktex[3]:
+        	self.x=self.schnittpunktex[1]
+        elif self.schnittpunktex[2]==self.schnittpunktex[3]:
+        	self.x=self.schnittpunktex[2]
+        else :
+        	self.x=self.x1
+        
+        if self.schnittpunktey[0]==self.schnittpunktey[1] or self.schnittpunktey[0]==self.schnittpunktey[2] or self.schnittpunktey[0]==self.schnittpunktey[3]:
+        	self.y=self.schnittpunktey[0]
+        elif self.schnittpunktey[2]==self.schnittpunktey[1] or self.schnittpunktey[1]==self.schnittpunktey[3]:
+        	self.y=self.schnittpunktey[1]
+        elif self.schnittpunktey[2]==self.schnittpunktey[3]:
+        	self.y=self.schnittpunktey[2]
+        else :
+        	self.y=self.y1
+        
+       
+        
+        self.get_logger().info(f'{self.schnittpunktex[0]}')
+        self.get_logger().info(f'{self.schnittpunktex[1]}')
+        self.get_logger().info(f'{self.schnittpunktex[2]}')
+        self.get_logger().info(f'{self.schnittpunktex[3]}')
+        #self.get_logger().info(f'{x}')
+        self.schnittpunktex.clear()
+        self.schnittpunktey.clear()
+
+        #x = np.mean([r.range for r in self.anchor_ranges]) - 0.5
+        
+     
+        self.x1=self.x
+        self.y1=self.y
+        #self.get_logger().info(f'{x}')
+        return self.x, self.y, 0.5
 
 
 def main(args=None):
